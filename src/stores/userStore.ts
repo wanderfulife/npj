82074da98e60ab56f20eddd08293635ac7c9fd45
J Wander <wander@Js-MacBook-Pro.local>
2024-09-27 09:14:14 +0200

    store

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -4,15 +4,18 @@ export const useUserStore = defineStore('user', {
   state: () => ({
     isAuthenticated: false,
     username: '',
+    email: '', // Add this line
   }),
   actions: {
-    login(username: string) {
+    login(username: string, email: string) { // Update this line
       this.isAuthenticated = true;
       this.username = username;
+      this.email = email; // Add this line
     },
     logout() {
       this.isAuthenticated = false;
       this.username = '';
+      this.email = ''; // Add this line
     },
   },
 });
